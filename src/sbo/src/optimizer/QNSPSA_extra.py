ac8c1dd3249de1303a070e1165b9a16c2b5ade32
JRenoir <observernicholas@gmail.com>
2025-08-10 20:05:28 -0500

    Add src folder from WISER_Optimization_VG

---
+++ src/sbo/src/optimizer/QNSPSA_extra.py
@@ -0,0 +1,6 @@
+def qnspsa_params():
+    return {'maxiter': 100,              
+    'blocking': True,
+    'learning_rate': None,       # Auto-calibrated
+    'perturbation': None,        # Auto-calibrated
+    'resamplings': 1,}
